78f86b80b18f824e0ddd8fbf2d032a630ed4f27c
Aishwarya B <baish997@gmail.com>
2022-06-12 20:23:40 +0530

    Rules added

--- GeneratePassword.py
+++ GeneratePassword.py
@@ -67,3 +67,135 @@ def getNextChar(c, forward):
 import string
 digs = string.digits + string.ascii_letters
 
+def Rule1(passwd):
+    '''
+    Alternate Characters Increment Decrement
+    Example - hello -> idmkp
+    '''
+    debug("Rule1")
+    new_pass = ""
+    for i in range(len(passwd)):
+        if(i % 2 == 0):
+            new_pass += getNextChar(passwd[i], True)
+        else:
+            new_pass += getNextChar(passwd[i], False)
+    debug(new_pass)
+    debug("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+    return new_pass
+
+def Rule2(passwd):
+    '''
+    Take successive character of every character
+    Example - hello -> ifmmp
+    '''
+    debug("Rule2")
+    new_pass = ''.join(list(map(lambda x: getNextChar(x, True), list(passwd))))
+    debug(new_pass)
+    debug("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+    return new_pass
+
+def Rule3(passwd):
+    '''
+    Take previous character of every character
+    Example - hello -> gdkkn
+    '''
+    debug("Rule3")
+    new_pass = ''.join(list(map(lambda x: getNextChar(x, False), list(passwd))))
+    debug(new_pass)
+    debug("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+    return new_pass
+
+
+def Rule4(passwd):
+    '''
+    Take successive character of every alternate character
+    Example - hello -> iemlp
+    '''
+    debug("Rule4")
+    def f(i):
+        if(i % 2 == 0):
+            return getNextChar(passwd[i], True)
+        else:
+            return passwd[i]
+
+    new_pass = ''.join([f(i) for i in range(len(passwd))])
+    debug(new_pass)
+    debug("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+    return new_pass
+
+def Rule5(passwd):
+    '''
+    Take previous character of every alternate character
+    Example - hello -> gekln
+    '''
+    debug("Rule5")
+    def f(i):
+        if(i % 2 == 0):
+            return getNextChar(passwd[i], False)
+        else:
+            return passwd[i]
+    new_pass = ''.join([f(i) for i in range(len(passwd))])
+    debug(new_pass)
+    debug("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+    return new_pass
+
+def Rule6(passwd):
+    '''
+    Take successive character of every character at position
+    indicated by your random number sequence
+    '''
+    debug("Rule6")
+    my_seq = []
+    for i in seq:
+        my_seq.append(i % len(passwd))
+
+    def f(i):
+        if(i + 1 in my_seq):
+            return getNextChar(passwd[i], True)
+        else:
+            return passwd[i]
+
+    new_pass = ''.join([f(j) for j in range(len(passwd))])
+    debug(new_pass)
+    debug("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+    return new_pass
+
+def Rule7(passwd):
+    '''
+    Take previous character of every character at position
+    indicated by your random number sequence
+    '''
+    debug("Rule7")
+    my_seq = []
+    for i in seq:
+        my_seq.append(i % len(passwd))
+    def f(i):
+        if(i + 1 in my_seq):
+            return getNextChar(passwd[i], False)
+        else:
+            return passwd[i]
+    new_pass = ''.join([f(i) for i in range(len(passwd))])
+    debug(new_pass)
+    debug("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+    return new_pass
+
+def Rule8(passwd):
+    '''
+    Replace characters at even places by a particular symbols/alphabets/number - Easy
+    Example - hello ->  h@l@o  (Replacing even places by symbol @)
+    '''
+    debug("Rule8")
+    '''
+    p = list(passwd)
+    p[::2] = [symbol] * (len(passwd)//2)
+    new_pass = ''.join(p)
+    '''
+    new_pass = ""
+    for i in range(len(passwd)):
+        if(i % 2 == 0):
+            new_pass += symbol
+        else:
+            new_pass += passwd[i]
+    debug(new_pass)
+    debug("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
+    return 
